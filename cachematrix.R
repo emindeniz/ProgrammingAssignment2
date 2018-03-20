c34f7b47c373f558fa5a7e0ee490c2d3786b2287
Ozkan <eozkan@us.imshealth.com>
2018-03-20 17:03:17 -0400

    answer

--- cachematrix.R
+++ cachematrix.R
@@ -10,8 +10,8 @@ makeCacheMatrix <- function(x = matrix()) {
     inverse <<- NULL
   }
   get <- function() x
-  setmean <- function(inverse_m) inverse <<- inverse_m
-  getmean <- function() inverse
+  setinverse <- function(inverse_m) inverse <<- inverse_m
+  getinverse <- function() inverse
   list(set = set, get = get,
        setmean = setinverse,
        getmean = getinverse)
@@ -24,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   
   ## Return a matrix that is the inverse of 'x'
-  inverse <- x$getmean()
+  inverse <- x$getinverse()
   if(!is.null(inverse)) {
     message("getting cached data")
     return(inverse)
   }
   matrix_m <- x$get()
-  inverse <- solve(matrix_m, ...)
-  x$setmean(inverse)
+  inverse <- solve(matrix_m)
+  x$setinverse(inverse)
   inverse
 }
